f2c0a974ceba4cafa2734eed11b461739194ee61
Vraj <vrajpatel00222@gmail.com>
2024-08-07 13:19:52 -0700

    parsing json

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
+const { createTodo } =  require("./types");
 const port = 3000;
 
 app.use(express.json());
 
 app.post("/todo",function(req,res){
-
+    const createpayload = req.body;
+    const parsePayload = createTodo.safeParse(createpayload);
+    if(!parsePayload.success){
+        res.status(411).json({
+            msg:"you sent the wrong inputs",
+        })
+        return;
+    }
 })
 
 app.get("/todos",function(req,res){
@@ -13,7 +21,14 @@ app.get("/todos",function(req,res){
 })
 
 app.put("/completed",function(req,res){
-    
+    const createpayload = req.body;
+    const parsePayload = createTodo.safeParse(createpayload);
+    if(!parsePayload.success){
+        res.status(411).json({
+            msg:"you sent the wrong inputs",
+        })
+        return;
+    }
 })
 
 app.listen(port,()=>{
