9f75c9c1ac4b27c5c0aaa561c9436a5fdf139597
Vraj <vrajpatel00222@gmail.com>
2024-08-07 12:29:17 -0700

    Create endpoints

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+app.post("/todo",function(req,res){
+
+})
+
+app.get("/todos",function(req,res){
+    
+})
+
+app.put("/completed",function(req,res){
+    
+})
 
 app.listen(port,()=>{
     console.log(`TODO app listening on port ${port}`)
