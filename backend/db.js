a12310b1ce0609465c7cec9734eb22fb0e801847
Vraj <vrajpatel00222@gmail.com>
2024-08-08 10:28:04 -0700

    removed MongoDB url

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require ("mongoose");
 //mongodb+srv://vrajpatel00222:jdcJ1Iqy2GeTaGML@cluster0.xxfd7y9.mongodb.net/todos
-// mongoose.connect = ("mongodb+srv://vrajpatel00222:jdcJ1Iqy2GeTaGML@cluster0.xxfd7y9.mongodb.net/todos")
+
 
 const todoSchema = mongoose.Schema({
     title: String,
